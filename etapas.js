8f7a751e21b0fee27a734489a4d56d0ffdbc5ee8
Rubens Ramm <rubensramm@gmail.com>
2022-09-25 12:03:10 -0300

    add o projeto

---
+++ etapas.js
@@ -0,0 +1,51 @@
+let etapas = [
+    {
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                numero: '54628',
+                nome: 'Fulano de tal',
+                partido: 'ABC',
+                fotos: [
+                    { url: '77890.jpg', legenda: 'Vereador' }
+                ]
+            },
+            {
+                numero: '74256',
+                nome: 'Beltrano da Silva',
+                partido: 'DEFG',
+                fotos: [
+                    { url: '77456.jpg', legenda: 'Vereador' }
+                ]
+            },
+        ]
+
+    },
+    {
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '35',
+                nome: 'Ciclano',
+                partido: 'ABC',
+                vice: 'Cic',
+                fotos: [
+                    { url: '99.jpg', legenda: 'Prefeito' },
+                    { url: '99_2.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+            {
+                numero: '63',
+                nome: 'Zulano',
+                partido: 'QWERTY',
+                vice: 'Zul',
+                fotos: [
+                    { url: '84.jpg', legenda: 'Prefeito' },
+                    { url: '84_2.jpg', legenda: 'Vice-Prefeito', small: true }
+                ]
+            },
+        ]
+    }
+];
